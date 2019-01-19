44988a900343ad81f6dd8a5e1e13b8f2f502cca2
Sahil Sharma <sasharma@anviam.com>
2019-01-19 12:38:26 +0530

    Updates

---
+++ precache-manifest.d05453237840b0af93d7dbe12b3a5e69.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e8e44cd8afb211b3bfb5",
+    "url": "/static/css/main.bcc87772.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a5dae1e66e9400dc0ac1",
+    "url": "/static/js/1.a5dae1e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8e44cd8afb211b3bfb5",
+    "url": "/static/js/main.e8e44cd8.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a5dae1e66e9400dc0ac1",
+    "url": "/static/css/1.e709e90e.chunk.css"
+  },
+  {
+    "revision": "517421b564a4861f79da0ce4785b9e9d",
+    "url": "/index.html"
+  }
+];
